06e799b8fc08b047ab3681c36e586ffed5383b8d
PK1312 <gdawg3@gmail.com>
2016-04-03 19:14:28 -0700

    The actual solution
    Turns out it wasn't inclusive of 1000, so I changed it to reflect that fact. This one actually givesthe correct solution.
    
    Also I actually used the right code instead of the mess where I tried to use lambda for some reason.

--- Euler1.py
+++ Euler1.py
@@ -1,5 +1,8 @@
 """I'm attempting to find the sum of all multiples of 3 or 5 below 1000."""
-euler = []
-x = lambda x: filter(x in range(0,1001) if x % 3 == 0, euler)
+solution = []
 
-print x(3)
+for number in range(0,1000):
+    if number % 3 == 0 or number % 5 == 0:
+        solution.append(number)
+
+print sum(solution)
