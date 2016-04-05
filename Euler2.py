f6dfa5a58f93ea8e3e83904481d9f0e8a8940497
Grant <gdawg3@gmail.com>
2016-04-05 16:15:42 -0700

    Update Euler2.py
    I was pretty proud of this this one. False start into recursivity, but this came to me in the bathtub!

--- Euler2.py
+++ Euler2.py
@@ -1,14 +1,10 @@
-"""By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms."""
-x = int(raw_input("What position in the sequence? "))
-def fib(x):
-    result = 0
-    if x == 1:
-        result = 1
-    elif x == 0:
-        result = 0
-    else:
-        result = fib(x-1) + fib(x-2) #Okay, just for me: this is a recursive function. Basically, what's happening here is a recursive function. It's calling itself. If you were to call, say, fib(10), it would fall through the if/elif/else chain, then
-		#call fib(9) + fib(8)- then, since it can't do either of those, it calls fib(8) + fib(7) and so forth until it gets to fib(1)- which it CAN do, then work back up form there to fib(10)
-    return result
-
-print fib(x)
+fibbo = [0, 1, 1] #gotta have the first few digits
+answer = [] #empty list for storing the even numbers to be summed
+for number in fibbo: #iterate through every number in the fibbo list
+    while fibbo[-1] <= 4000000: #ensures it stops within the bounds of the problem
+        fibbo.append(fibbo[-1] + fibbo[-2]) #this grabs the last item in a list, then the second to last, adds them together
+        				    #then appends it to "fibbo"
+for number in fibbo: #iterates through everything in fibbo, now that it has a bunch of numbers in it
+    if number % 2 == 0: #checks if numbers are even
+        answer.append(number) #and if they are, appends them to answer
+print sum(answer) #adds 'em all together and bam! correct answer
